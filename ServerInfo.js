6533cce09a70be2ef9e38846766ddd80f1c4b139
FFGFlash <tayld.ainjastudios@gmail.com>
2017-04-03 13:19:36 -0500

    Update ServerInfo.js

--- ServerInfo.js
+++ ServerInfo.js
@@ -25,7 +25,7 @@ function updateServerInfoRefresh(StatusBoxID,ServerName,ServerIP,ServerPort,Show
 function useDefaultStatusBox(StatusBoxID) {
   $(function() {
     $(StatusBoxID).html(
-    "<div id='uStatus'><h3><span id='status-icon'></span> <span id='ip'></span></h3></div><div id='lStatus'>Status: <span id='status'></span><br>Players: <span id='players'></span></div>"
+    "<link rel='stylesheet' href='https://cdn.rawgit.com/FFGFlash/MCServerStatus/305e2920/stylesheet.css'/><div id='uStatus'><h3><span id='status-icon'></span> <span id='ip'></span></h3></div><div id='lStatus'>Status: <span id='status'></span><br>Players: <span id='players'></span></div>"
     );
   });
 }
