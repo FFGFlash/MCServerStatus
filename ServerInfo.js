810a22e9ea8a2b20bfbb9f39be1ba561406d62e1
FFGFlash <tayld.ainjastudios@gmail.com>
2017-04-03 11:45:08 -0500

    Update ServerInfo.js

--- ServerInfo.js
+++ ServerInfo.js
@@ -1,35 +1,31 @@
-function getServerStatus(StatusBoxID,ServerName,ServerIP,ServerPort,ShowIP) {
-  $(document).ready(function() {
+function updateServerInfo(StatusBoxID,ServerName,ServerIP,ServerPort,ShowIP) {
+  $(function() {
     $.ajax({
       url: 'https://mcapi.us/server/status?ip='+ServerIP+'&port='+ServerPort,
       dataType: 'text',
       success: function(data) {
         var json = $.parseJSON(data);
-        if(ShowIP) {
-          $(StatusBoxID).children('#ip').html(ServerIP+":"+ServerPort);
-        } else {
-          $(StatusBoxID).children('#ip').html(ServerName);
-        }
-        $(StatusBoxID).children('#name').html(ServerName);
-        $(StatusBoxID).children('#status').html(json.online ? 'online':'offline');
-        $(StatusBoxID).children('#status-icon').html(json.online ? '<img width="17" height="17" src="https://cdn.rawgit.com/FFGFlash/MCServerStatus/01609718/online.png"/>':'<img width="17" height="17" src="https://cdn.rawgit.com/FFGFlash/MCServerStatus/d97e8033/offline.png"/>');
-        $(StatusBoxID).children('#players').html(json.players.now+"/"+json.players.max);
-        $(StatusBoxID).children('#current-players').html(json.players.now);
-        $(StatusBoxID).children('#max-players').html(json.players.max);
+        if(ShowIP)
+          $(StatusBoxID).find("#ip").html(ServerIP+":"+ServerPort);
+        $(StatusBoxID).find("#name").html(ServerName);
+        $(StatusBoxID).find("#status").html(json.online ? 'online':'offline');
+        $(StatusBoxID).find("#status-icon").html(json.online ? '<img width="17" height="17" src="https://cdn.rawgit.com/FFGFlash/MCServerStatus/01609718/online.png"/>':'<img width="17" height="17" src="https://cdn.rawgit.com/FFGFlash/MCServerStatus/d97e8033/offline.png"/>');
+        $(StatusBoxID).find("#players").html(json.players.now+"/"+json.players.max);
+        $(StatusBoxID).find("#current-players").html(json.players.now);
+        $(StatusBoxID).find("#max-players").html(json.players.max);
       }
     });
   });
 }
 
-function getServerStatusLU(StatusBoxID,ServerName,ServerIP,ServerPort,ShowIP) {
-  setInterval(getServerStatus.bind(null,StatusBoxID,ServerName,ServerIP,ServerPort,ShowIP),1000);
+function updateServerInfoRefresh(StatusBoxID,ServerName,ServerIP,ServerPort,ShowIP,RefreshRate) {
+  setInterval(updateServerInfo.bind(null,StatusBoxID,ServerName,ServerIP,ServerPort,ShowIP),RefreshRate);
 }
 
-function defaultStatusBox(StatusBoxID) {
-  $(document).ready(function() {
+function useDefaultStatusBox(StatusBoxID) {
+  $(function() {
     $(StatusBoxID).html(
     "<div id='uStatus'><h3><span id='status-icon'></span> <span id='ip'></span></h3></div><div id='lStatus'>Status: <span id='status'></span><br>Players: <span id='players'></span></div>"
     );
-    return StatusBoxID;
   });
 }
